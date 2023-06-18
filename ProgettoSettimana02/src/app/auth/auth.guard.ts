131eb5a3bdbb9652ea115745f3fe868aa24799b1
rositab91 <rositab91@live.it>
2023-06-18 19:29:15 +0200

    ProgettoSettimana02

---
+++ ProgettoSettimana02/src/app/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(private authSrv: AuthService, private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ):
+        | boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.authSrv.user$.pipe(
+            take(1),
+            map((user) => {
+                if (user) {
+                    return true;
+                }
+                return this.router.createUrlTree(['/login'])
+            })
+        )
+    }
+}
